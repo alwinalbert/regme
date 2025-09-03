b8d1ffa79000f2a02144e0010fa9b9a6bd06d634
Alwiii <alwinalbert70@gmail.com>
2025-09-03 10:57:59 +0530

    welcome screen setted up

---
+++ app/lib/screens/signup_screen.dart
@@ -0,0 +1,11 @@
+import 'package:app/widgets/custom_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScaffold();
+  }
+}
---
+++ app/lib/screens/welcome_screen.dart
@@ -0,0 +1,49 @@
+import 'package:app/widgets/custom_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      child: Column(
+        children: [
+          Flexible(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 0,
+                horizontal: 40.0,
+              ),
+              child: Center(
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'HI!\n',
+                        style: TextStyle(
+                          fontSize: 45,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Welcome to Hall Booking enter your details',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
